09a512710545ccd21067777644359a526b19d48c
GenesisTeodoro <gjuteodoro010@gmail.com>
2020-02-22 23:51:30 +0800

    personal form - assignment2

---
+++ InformationForm/InformationForm/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(200, 33)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(369, 34)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(83, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 28)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Name :"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(200, 85)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(369, 34)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(83, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 28)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Age :"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(200, 135)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(369, 52)
+        Me.TextBox3.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(83, 135)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 28)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Address :"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(120, 211)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(107, 41)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "TEXT FILE"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(288, 211)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(107, 41)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "XML"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(462, 211)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(107, 41)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "JSON"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(84, 280)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 21)
+        Me.Label4.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(751, 332)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label4 As Label
+End Class
---
+++ InformationForm/InformationForm/Form1.vb
@@ -0,0 +1,85 @@
+﻿Imports System.IO
+Imports System.DateTime
+Imports System.Xml
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+
+Public Class Form1
+    Public Property today As String = Date.Now.ToString("yyyy-MM-dd HH-mm-ss")
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim txtFile As System.IO.StreamWriter
+        Dim info As Info = New Info()
+        txtFile = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\G\source\repos\InformationForm\InformationForm\ExportedFiles\" & "TextFile - " & today & ".txt", True)
+
+        info.getInfo(TextBox1.Text, TextBox2.Text, TextBox3.Text)
+
+        Dim output As String = "name : " & info.name & vbCrLf &
+                               "age : " & info.age & vbCrLf &
+                               "address : " & info.address & vbCrLf
+
+
+        Label4.Text = "Check the file at C:\Users\G\source\repos\InformationForm\InformationForm\ExportedFiles\"
+
+        txtFile.WriteLine(output)
+        txtFile.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim info As Info = New Info()
+        info.getInfo(TextBox1.Text, TextBox2.Text, TextBox3.Text)
+
+        Dim settings As XmlWriterSettings = New XmlWriterSettings()
+        settings.Indent = True
+
+        Using writer As XmlWriter = XmlWriter.Create("C:\Users\G\source\repos\InformationForm\InformationForm\ExportedFiles\" & "XmlFile - " & today & ".xml", settings)
+            writer.WriteStartDocument()
+            writer.WriteStartElement("Person")
+
+            writer.WriteStartElement("Info")
+            writer.WriteElementString("name", info.name)
+            writer.WriteElementString("age", info.age)
+            writer.WriteElementString("address", info.address)
+            writer.WriteEndElement()
+
+            writer.WriteEndElement()
+            writer.WriteEndDocument()
+        End Using
+
+        Label4.Text = "Check the file at C:\Users\G\source\repos\InformationForm\InformationForm\ExportedFiles\"
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim info As Info = New Info()
+        info.getInfo(TextBox1.Text, TextBox2.Text, TextBox3.Text)
+
+        Dim jsonString As String = JsonConvert.SerializeObject(info)
+
+        Dim file As StreamWriter = My.Computer.FileSystem.OpenTextFileWriter("C:\Users\G\source\repos\InformationForm\InformationForm\ExportedFiles\" & "JsonFile - " & today & ".json", True)
+        file.WriteLine(jsonString)
+        file.Close()
+
+        Label4.Text = "Check the file at C:\Users\G\source\repos\InformationForm\InformationForm\ExportedFiles\"
+    End Sub
+End Class
+
+Class Info
+    Public Property name As String
+    Public Property age As Byte
+    Public Property address As String
+
+
+    Public Sub getInfo(ByVal txtbox1 As String, ByVal txtbox2 As Byte, ByVal txtbox3 As String)
+
+        name = txtbox1
+        age = txtbox2
+        address = txtbox3
+
+
+    End Sub
+End Class
